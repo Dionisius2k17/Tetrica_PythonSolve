e64f9241d59d53794105a5682457f4adeaf6f7de
Dionisius2k17 <34659280+Dionisius2k17@users.noreply.github.com>
2021-03-19 13:03:30 +0300

    1st task is done

---
+++ task_1.py
@@ -0,0 +1,9 @@
+def task(array):
+    i = 0
+    while (i <= len(array)):
+        if (array[i] == '0'):
+            print("First zero index is", i)
+            break
+        i += 1
+
+print(task("111111111111111111111111100000000")) 
